d323238d92c64820949d266212515083bd4f6e44
Fernando Cardozo <nuevo_10000@outlook.com>
2024-01-11 19:33:20 -0300

    feat: exercise two repeatString completed

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,23 @@
-const repeatString = function() {
-
+const repeatString = function(text, number) {
+  let i = 1;
+  finalText = '';
+  if (number < 0)
+  {
+    return 'ERROR';
+  }
+  else if (number == 0 || text.length == '')
+  {
+    return '';
+  }
+  else
+  {
+    while (i <= number)
+    {
+      finalText = finalText + text;
+      i = i + 1;
+    }
+    return finalText;
+  }
 };
 
 // Do not edit below this line
