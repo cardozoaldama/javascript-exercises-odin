43e435f6e21b26fc0c9964182123aed441aa5b3c
Fernando Cardozo <nuevo_10000@outlook.com>
2024-01-15 15:57:09 -0300

    feat: exercise 4 completed

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,5 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(theArray, ...theArgs) {
+ return theArray.filter(val => !theArgs.includes(val))
 };
 
 // Do not edit below this line
