46de42d1adc3e77c0cf2c86892465140479d366d
Fernando Cardozo <nuevo_10000@outlook.com>
2024-01-15 15:00:09 -0300

    feat: test of the exercise 3 completed

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,10 @@
-const reverseString = function() {
-
+const reverseString = function(text) {
+  let finalText = '';
+  for(let i = 1; i <= text.length; i++)
+  {
+    finalText += text.at(-i);
+  }
+  return finalText;
 };
 
 // Do not edit below this line
