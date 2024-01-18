e8ebb01573bd80c611fbf9864270fc9bfaafb46b
Fernando Cardozo <nuevo_10000@outlook.com>
2024-01-18 12:20:30 -0300

    feat: exercise 7 has been completed

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,13 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(fahr) {
+  let result = (fahr - 32) * 5/9;
+  result = result.toFixed(1);
+  return parseFloat(result);
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(celsius) {
+  let result = (celsius * 9/5) + 32;
+  result = result.toFixed(1);
+  return parseFloat(result);
 };
 
 // Do not edit below this line
